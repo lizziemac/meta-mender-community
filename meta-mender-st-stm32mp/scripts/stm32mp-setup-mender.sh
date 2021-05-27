bf5afbe5fc58a62a27480af830752f1ccccdbfb0
Liz MacLean <elizabeth.maclean95@gmail.com>
2021-05-27 19:21:24 -0400

    Add small cheat to init script to fix bad path in meta-mender-core gptimg call

--- meta-mender-st-stm32mp/scripts/stm32mp-setup-mender.sh
+++ meta-mender-st-stm32mp/scripts/stm32mp-setup-mender.sh
@@ -13,6 +13,9 @@ echo "ADDONSLAYERS += \"\${@'\${OEROOT}/layers/meta-mender-community/meta-mender
 cat ../layers/meta-mender-community/templates/local.conf.append >> conf/local.conf
 cat ../layers/meta-mender-community/meta-mender-st-stm32mp/templates/local.conf.append >> conf/local.conf
 
+# STRATIS workaround instead of also having to manage meta-mender-core
+sed -i 's@${DEPLOY_DIR_IMAGE}/uboot.env@${DEPLOY_DIR_IMAGE}/u-boot/uboot.env@g' ./layers/meta-mender/meta-mender-core/classes/mender-part-images.bbclass
+
 echo ""
 echo "Mender integration complete."
 echo ""
